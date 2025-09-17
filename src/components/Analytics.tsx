53fde09507117d2aa9c5d6e22c82877eabb2ecc2
Vatsal Patel <73291384+vatsalpatel1123@users.noreply.github.com>
2025-09-17 14:35:41 +0530

    Create Analytics.tsx

---
+++ src/components/Analytics.tsx
@@ -0,0 +1,19 @@
+// components/Analytics.tsx
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const Analytics = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (typeof window.gtag === 'function') {
+      window.gtag('config', 'G-LN89N95JRP', {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
+
+  return null;
+};
+
+export default Analytics;
