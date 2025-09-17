f4e612f5513d07220296a5c6dd2b39951898f369
Vatsal Patel <73291384+vatsalpatel1123@users.noreply.github.com>
2025-09-17 14:37:44 +0530

    Update Analytics.tsx

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,13 +1,18 @@
-// components/Analytics.tsx
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
 
 const Analytics = () => {
   const location = useLocation();
 
   useEffect(() => {
-    if (typeof window.gtag === 'function') {
-      window.gtag('config', 'G-LN89N95JRP', {
+    if (typeof window.gtag === "function") {
+      window.gtag("config", "G-LN89N95JRP", {
         page_path: location.pathname + location.search,
       });
     }
